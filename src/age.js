e989228d043286978fd84cc3ca10fc599dff96de
Eva Wiedmann <evawiedmann@gmail.com>
2019-11-15 15:38:43 -0800

    class broken down into two, more tests added

--- src/age.js
+++ src/age.js
@@ -1,5 +1,5 @@
 export class Person {
-  constructor(currentAge, mercury, venus, mars, jupiter, timeLeft) {
+  constructor(currentAge, mercury, venus, mars, jupiter) {
     this.currentAge = currentAge;
     this.mercury = maxAge;
     this.venus = venus;
@@ -22,32 +22,29 @@ export class Person {
   jupiterYears() {
     return `Your age is ${this.currentAge * 11.86} years on Jupiter!`;
   };
+};
+
+export class Hourglass {
+  constructor(remain, remainMercury, remainVenus, remainMars, remainJupiter) {
+    this.remain = remain;
+    this.remainMercury = remainMercury;
+    this.remainVenus = remainVenus;
+    this.remainMars = remainMars;
+    this.remainJupiter = remainJupiter;
+  }
   remainingYearsOnEarth() {
-    return `You have ${this.currentAge} years left on Earth!`;
+    return `You have (80 - ${this.currentAge}) years left on Earth!`;
   };
   remainingYearsOnMercury() {
-    return `Your age is ${this.currentAge * .24} years on Mercury!`;
+    return `Your age is ((80 - ${this.currentAge}) * .24) years on Mercury!`;
   };
   remainingYearsOnVenus() {
-    return `You have ${this.currentAge * .62} years left on Venus!`;
+    return `You have ((80 - ${this.currentAge}) * .62) years left on Venus!`;
   };
   remainingYearsOnMars() {
-    return `You have ${this.currentAge * 1.88} years left on Mars!`;
+    return `You have ((80 - ${this.currentAge}) * 1.88) years left on Mars!`;
   };
   remainingYearsOnJupiter() {
-    return `You have ${this.currentAge * 11.86} years left on Jupiter!`;
+    return `You have ((80 - ${this.currentAge} * 11.86) years left on Jupiter!`;
   };
-// }
-//
-// export class Age {
-//   constructor(type, name, skills) {
-//     this.type = type;
-//     this.name = name;
-//     this.skills = skills;
-//   }
-//   chooseType() {
-//     // return 'You have chosen ' + this.type;
-//     return `You have chosen ${this.type}`
-//   }
-//
-//    }
+};
